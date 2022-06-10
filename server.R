baffbd73489d4f10d1afb738de9b3179875c0909
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-06-10 12:19:18 -0400

    mutate update

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ server <- function(input,output,session){
     isolate({
     for(bet in 1:length(unique(fifa_df$award))){
       temp_dt <- datatable(rv$data %>% dplyr::filter(award == unique(fifa_df$award)[bet]) %>%
-                             dplyr::mutate(payout == input$bet_size *(odds/100)))
+                             dplyr::mutate(payout = (input$bet_size *(odds/100))))
       rv$odds_ui <- tagList(rv$odds_ui,temp_dt) 
     }
     })
