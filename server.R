ce60dc72ec6f461f752dcc7b38fd154d46947fba
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-06-10 12:33:09 -0400

    updating from bet_size

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ server <- function(input,output,session){
   output$odds_ui <- renderUI({
     
     rv$odds_ui <- tagList()
+    rv$bet_size <- input$bet_size
     
     isolate({
     for(bet in 1:length(unique(fifa_df$award))){
@@ -14,5 +15,17 @@ server <- function(input,output,session){
     })
     
     tagList(rv$odds_ui)
+  })
+  
+  observeEvent(input$bet_size,{
+    
+    rv$odds_ui <- tagList()
+    rv$bet_size <- input$bet_size
+    
+      for(bet in 1:length(unique(fifa_df$award))){
+        temp_dt <- datatable(rv$data %>% dplyr::filter(award == unique(fifa_df$award)[bet]) %>%
+                               dplyr::mutate(payout = (input$bet_size *(odds/100))))
+        rv$odds_ui <- tagList(rv$odds_ui,temp_dt) 
+      }
   })
 }
