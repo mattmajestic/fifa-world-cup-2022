bea447afe4fcdbbf6c9e9565f396a5b30f53343b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-06-10 08:52:14 -0400

    adding basic app

--- global.R
+++ global.R
@@ -1,3 +1,14 @@
 library(shiny)
 library(shinydisconnect)
 library(shinybrowser)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinyjs)
+library(waiter)
+library(DT)
+library(ggplot2)
+library(plotly)
+
+summary_tag <- h3("Welcome to the Fifa World Cup 2022 App for looking at some of the favored odds I have on the upcoming world up")
+
+fifa_df <- read.csv("fifa-world-cup-2022.csv",stringsAsFactors = FALSE)
--- server.R
+++ server.R
@@ -1,3 +1,17 @@
 server <- function(input,output,session){
-  rv <- reactiveValues()
+  rv <- reactiveValues(data = fifa_df)
+  
+  output$odds_ui <- renderUI({
+    
+    output$odds_plot <- renderPlotly({
+      ggplot(rv$data, aes(x=player, y=odds, fill=player))+
+        geom_bar(stat='identity')+
+        facet_grid(~award, scales="free_y")
+      
+    })
+    
+    tagList(
+      plotlyOutput("odds_plot")
+    )
+  })
 }
